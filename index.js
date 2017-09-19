66284e9ec2cec7d4ad999e76912c8e646a7b5e74
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2017-09-19 15:59:26 +0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.handler = (event, context, callback) => {
+    console.log('Running index.handler');
+    console.log('==================================');
+    console.log('event', event);
+    console.log('==================================');
+    console.log('Stopping index.handler');
+    callback(null, event);
+    // or
+    // callback( 'some error type' );
+};
