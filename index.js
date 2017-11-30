4e22106987c02c7b5d7ac0597772bedd6ebfffa5
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2017-11-30 12:15:15 +0200

    initial

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-exports.handler = (event, context, callback) => {
-    console.log('Running index.handler');
-    console.log('==================================');
-    console.log('event', event);
-    console.log('==================================');
-    console.log('Stopping index.handler');
-    callback(null, event);
-    // or
-    // callback( 'some error type' );
-};
+let penthouse = require('penthouse');
+let fetch = require('node-fetch');
+let CleanCSS = require('clean-css');
+
+exports.handler = ({
+    key,
+    styles,
+    url,
+    hash,
+    return_url,
+    _wpnonce
+}, context, callback) => Promise.all(styles.map(fetch))
+    .then(responses => Promise.all(responses.map(res => res.text())))
+    .then(cssStrings => penthouse({
+        url,
+        cssString: cssStrings.join(' ')
+    }))
+    .then(criticalCss => fetch(return_url, {
+        method: `POST`,
+        body: [
+            `key=${key}`,
+            `hash=${hash}`,
+            `stylesheet=${new CleanCSS().minify(criticalCss).styles}`
+        ].join('&')
+    }));
